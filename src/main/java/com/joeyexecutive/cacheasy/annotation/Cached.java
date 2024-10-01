1d5e2bd841267cdd0646b3c958958619739bee78
Joey <b3tanyan@gmail.com>
2024-10-01 02:27:28 -0400

    Annotate the annotation

--- src/main/java/com/joeyexecutive/cacheasy/annotation/Cached.java
+++ src/main/java/com/joeyexecutive/cacheasy/annotation/Cached.java
@@ -1,7 +1,13 @@
 package com.joeyexecutive.cacheasy.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
 public @interface Cached {
 
     Class<?> cacheProvider();
